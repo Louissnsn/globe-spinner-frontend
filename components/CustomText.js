0e145d3f79f1aff16913417fbc795f09858eee81
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-07-12 12:52:05 +0200

    clean + comments CustomText

--- components/CustomText.js
+++ components/CustomText.js
@@ -1,20 +1,25 @@
+import React from "react";
 import { Text as CustomRNText, StyleSheet } from "react-native";
 import { useFonts, KronaOne_400Regular } from "@expo-google-fonts/krona-one";
 import { NunitoSans_400Regular } from "@expo-google-fonts/nunito-sans";
 
+// CustomText component to display text with custom fonts and styles
 export const CustomText = ({ style, ...props }) => {
   const [fontsLoaded] = useFonts({
     KronaOne_400Regular,
     NunitoSans_400Regular,
   });
 
+  // Display loading indicator while fonts are loading
   if (!fontsLoaded) {
-    return <CustomRNText>:hourglass_flowing_sand:</CustomRNText>; // or loading spinner
+    return <CustomRNText>⏳</CustomRNText>; // or a loading spinner
   }
 
+  // Return text with combined styles
   return <CustomRNText style={[styles.text, style]} {...props} />;
 };
 
+// Styles for the custom text component
 const styles = StyleSheet.create({
   text: {
     fontFamily: "NunitoSans_400Regular",
