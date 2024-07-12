d931fb702a0cda76002aa451257697940a7e1c0b
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-07-12 14:08:08 +0200

    clean and modify SigninForm comp + comments

--- components/SigninForm.js
+++ components/SigninForm.js
@@ -1,38 +1,26 @@
 import React, { useState } from "react";
 import {
   View,
-  Modal,
   Text,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  Image,
   KeyboardAvoidingView,
   SafeAreaView,
   Alert,
+  Platform,
 } from "react-native";
-import { useSelector, useDispatch } from "react-redux";
-
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import GradienFontColor from "../components/GradientFontColor";
-import { ipAddress } from "../myVariables";
+import GradientFontColor from "../components/GradientFontColor";
 
 export default function SigninForm({ submit, closeModal }) {
-  //const userInfo = useSelector(state => state.userInfo.value);
-  //console.log(userInfo.isConnected);
-
-  //const dispatch = useDispatch();
-
-  const checkHasEmptyField = (fields) => {
-    for (let field of fields) {
-      if (!field || field === " ") return true;
-    }
-    return false;
-  };
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // Function to check if any fields are empty
+  const checkHasEmptyField = (fields) => fields.some((field) => !field.trim());
+
+  // Handle form submission
   const handlePressSubmit = async () => {
     if (checkHasEmptyField([email, password])) {
       Alert.alert("Some fields are missing!");
@@ -44,7 +32,6 @@ export default function SigninForm({ submit, closeModal }) {
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
-        enabled={true}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
         <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
@@ -52,9 +39,9 @@ export default function SigninForm({ submit, closeModal }) {
         </TouchableOpacity>
 
         <View style={styles.title}>
-          <GradienFontColor>
+          <GradientFontColor>
             <Text style={styles.signInText}>Sign in</Text>
-          </GradienFontColor>
+          </GradientFontColor>
         </View>
 
         <View style={styles.inputsContainer}>
@@ -62,10 +49,10 @@ export default function SigninForm({ submit, closeModal }) {
             <FontAwesome name="user" size={30} color="#BA99FE" />
             <View style={styles.textInputContainer}>
               <TextInput
-                placeholder="email"
+                placeholder="Email"
                 style={styles.textInput}
                 value={email}
-                onChangeText={(text) => setEmail(text)}
+                onChangeText={setEmail}
               />
             </View>
           </View>
@@ -74,19 +61,19 @@ export default function SigninForm({ submit, closeModal }) {
             <FontAwesome name="lock" size={30} color="#BA99FE" />
             <View style={styles.textInputContainer}>
               <TextInput
-                placeholder="password"
+                placeholder="Password"
                 style={styles.textInput}
                 value={password}
-                onChangeText={(text) => setPassword(text)}
-                secureTextEntry={true}
-              ></TextInput>
+                onChangeText={setPassword}
+                secureTextEntry
+              />
             </View>
           </View>
         </View>
       </KeyboardAvoidingView>
 
       <TouchableOpacity style={styles.submitButton} onPress={handlePressSubmit}>
-        <Text style={{ fontSize: 25, color: "white" }}>Submit</Text>
+        <Text style={styles.submitButtonText}>Submit</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -99,23 +86,33 @@ const styles = StyleSheet.create({
     width: "100%",
     flex: 1,
   },
-
   closeButton: {
     width: 40,
     height: 40,
     right: -20,
     top: 15,
-    marginTop: -15,
     position: "absolute",
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    alignItems: "center",
+    marginBottom: 80,
+    marginTop: 50,
+  },
+  signInText: {
+    fontSize: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "KronaOne_400Regular",
+    marginTop: 50,
+    marginBottom: 30,
   },
   inputsContainer: {
     width: "80%",
     marginVertical: 20,
     justifyContent: "center",
     alignItems: "center",
-    // flexWrap: "wrap",
   },
   textAndInput: {
     width: "100%",
@@ -123,7 +120,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 10,
   },
-
   textInputContainer: {
     fontSize: 16,
     borderBottomColor: "#BA99FE",
@@ -133,16 +129,8 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     flexDirection: "row",
   },
-
-  title: {
-    alignItems: "center",
-    marginBottom: 80,
-    marginTop: 50,
-  },
-
   textInput: {
     width: "100%",
-    borderWidth: 0,
     fontSize: 15,
     padding: 5,
     borderRadius: 20,
@@ -163,18 +151,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 60,
   },
-
   submitButtonText: {
     color: "#FFFFFF",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  signInText: {
-    fontSize: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    fontFamily: "KronaOne_400Regular",
-    marginTop: 50,
-    marginBottom: 30,
+    fontSize: 25,
   },
 });
