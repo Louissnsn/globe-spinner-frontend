abd7cc05cd29805dc1679baba22664487634a7bc
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-07-12 12:49:33 +0200

    clean useFetchSequence hook + comments

--- hooks/useFetchSequence.js
+++ hooks/useFetchSequence.js
@@ -1,76 +1,96 @@
-import { useState, useEffect } from 'react';
-import { useIsFocused } from '@react-navigation/native';
+import { useState, useEffect } from "react";
+import { useIsFocused } from "@react-navigation/native";
 
 const DEFAULT_OPTIONS = {
-	method: 'GET',
-	headers: { 'Content-Type': 'application/json' },
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
 };
 
 // To manage several fetches where the URL of the next fetch
 // depends on the progress info (including data) of all the fetches until now
 
-export default function useFetchSequence({ firstURL, triggerFirstFetch, requestsSequence }) {
-    
-	const [nextURL, setNextURL] = useState(firstURL);
-	const [requestIndex, setRequestIndex] = useState(0);
-	
-	const n = requestsSequence.length;
-	const isScreenFocused = useIsFocused();
-	const [requestsProgress, setRequestsProgress] = useState(Array(n).fill({
-		data: null,
-		error: null,
-		isLoading: false,
-	}));
-	
-	const updatedRequests = (prevRequests, keyValuePairs) => {
-		const requestsCopy = [...prevRequests];
-		requestsCopy[requestIndex] = { ...requestsCopy[requestIndex], ...keyValuePairs };
-		return requestsCopy;
-	}
-	
-	useEffect(() => {
-		const request = requestsSequence[requestIndex];
-		const { options=DEFAULT_OPTIONS, getNextURL } = request;
-		// getNextURL is a callback in the form: (requestsProgress) => return nextURL;
+export default function useFetchSequence({
+  firstURL,
+  triggerFirstFetch,
+  requestsSequence,
+}) {
+  const [nextURL, setNextURL] = useState(firstURL);
+  const [requestIndex, setRequestIndex] = useState(0);
 
-		const requestController = new AbortController();
+  const n = requestsSequence.length;
+  const isScreenFocused = useIsFocused();
+  const [requestsProgress, setRequestsProgress] = useState(
+    Array(n).fill({
+      data: null,
+      error: null,
+      isLoading: false,
+    })
+  );
 
-		if (!isScreenFocused) return;
+  // Function to update the requestsProgress array
+  const updatedRequests = (prevRequests, keyValuePairs) => {
+    const requestsCopy = [...prevRequests];
+    requestsCopy[requestIndex] = {
+      ...requestsCopy[requestIndex],
+      ...keyValuePairs,
+    };
+    return requestsCopy;
+  };
 
-		setRequestsProgress(prev => {
-			return updatedRequests(prev, { error: null, isLoading: true });
-		});
+  useEffect(() => {
+    const request = requestsSequence[requestIndex];
+    const { options = DEFAULT_OPTIONS, getNextURL } = request;
 
-		console.log('nextURL: ', nextURL);
+    const requestController = new AbortController();
 
-		fetch(nextURL, { ...options, signal: requestController.signal })
-			.then(response => {
-				if (!response.ok) {
-					setRequestsProgress(prev => {
-						return updatedRequests(prev, { error: response.statusText });
-					});
-					return;
-				}
-				return response.json();
-			})
-			.then(data => {
-				//setNextURL(getNextURL(data));
-				setNextURL(getNextURL(updatedRequests(requestsProgress, { data: data, isLoading: false })));
-				//console.log('[DATA]', data);
-				setRequestsProgress(prev => {
-					return updatedRequests(prev, { data: data, isLoading: false });
-				});
-				setRequestIndex(prev => prev+1);
-			})
-			.catch(error => {
-				setRequestsProgress(prev => {
-					return updatedRequests(prev, { error: error, isLoading: false });
-				});
-			});
+    if (!isScreenFocused) return;
 
-			return () => requestController.abort();
-		
-		}, [triggerFirstFetch, isScreenFocused, nextURL]);
+    // Set the current request to loading state
+    setRequestsProgress((prev) =>
+      updatedRequests(prev, { error: null, isLoading: true })
+    );
 
-	return requestsProgress;
+    console.log("nextURL: ", nextURL);
+
+    // Fetch data from the next URL
+    fetch(nextURL, { ...options, signal: requestController.signal })
+      .then((response) => {
+        if (!response.ok) {
+          setRequestsProgress((prev) =>
+            updatedRequests(prev, {
+              error: response.statusText,
+              isLoading: false,
+            })
+          );
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Update the progress and set the next URL
+        const updatedProgress = updatedRequests(requestsProgress, {
+          data: data,
+          isLoading: false,
+        });
+        setNextURL(getNextURL(updatedProgress));
+        setRequestsProgress(updatedProgress);
+        setRequestIndex((prev) => prev + 1);
+      })
+      .catch((error) => {
+        setRequestsProgress((prev) =>
+          updatedRequests(prev, { error: error.message, isLoading: false })
+        );
+      });
+
+    // Cleanup function to abort fetch on component unmount
+    return () => requestController.abort();
+  }, [
+    triggerFirstFetch,
+    isScreenFocused,
+    nextURL,
+    requestsSequence,
+    requestIndex,
+  ]);
+
+  return requestsProgress;
 }
